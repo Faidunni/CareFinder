31453d5a110fd6f3a825265ba31c3e6f35320bfb
Faidunni <hazeezfaizat@gmail.com>
2024-08-16 09:50:43 +0100

    input search

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -50,11 +50,11 @@ const Dashboard = ({ searchResult, setSearchResult }) => {
           lorem. Mattis volutpat venenatis facilisis orci facilisis eleifend
           elit amet.
         </p>
-        <div className="relative group w-[200px] mx-auto mb-7 sm:w-[400px] sm:mx-auto lg:mx-auto md:mx-auto -ml-2">
+        <div className="relative group w-full mx-auto mb-7 sm:w-[400px] sm:mx-auto lg:mx-auto md:mx-auto -ml-2">
           <input
             type="text"
             placeholder="Enter location, city or State"
-            className="w-[300px] sm:w-[400px]  rounded-full border border-gray-300 py-2 px-4 focus:outline-none focus:border-1 font-Lato font-4 shadow-lg "
+            className="w-full sm:w-[400px]  rounded-full border border-gray-300 py-2 px-4 focus:outline-none focus:border-1 font-Lato font-4 shadow-lg "
             onChange={handleInputChange}
             value={inputValue}
           />
@@ -64,7 +64,7 @@ const Dashboard = ({ searchResult, setSearchResult }) => {
               className="btn form-btn"
               onClick={handleSearch}
             >
-              <FaSearch className="text-secondary absolute bg-black pt-2 pb-2 pl-1 pr-1 text-3xl rounded-full border top-[5px] sm:right-[10px] -right-[45%]" />
+              <FaSearch className="text-secondary absolute bg-black pt-2 pb-2 pl-1 pr-1 text-3xl rounded-full border top-[5px] sm:right-[10px] left-[87%]" />
             </button>
           </NavLink>
         </div>
